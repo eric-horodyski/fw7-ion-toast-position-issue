3cccae15f09e1ab936c995b6e2f8b775fb0b864f
Eric Horodyski <horodyski@ionic.io>
2023-06-08 13:23:41 -0400

    Replicate issue

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -6,7 +6,10 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent {
-
   @Input() name?: string;
+  isToastOpen = false;
 
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
 }
