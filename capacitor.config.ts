028be7cad76a028752f321a22af6b09f87d2fd2d
Eric Horodyski <horodyski@ionic.io>
2023-06-08 12:37:53 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fw7-ion-toast-position-issue',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
